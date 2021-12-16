7d54b24023ce7727e58c64f024fe5bbe1f0f6baf
hameed <afg_ham@yahoo.com>
2021-12-15 23:29:24 -0700

    unit tests completed per PDF.  will implement methods soon.  also took a break between first check in and this one.

--- ShoppingCart/ShoppingCart.cs
+++ ShoppingCart/ShoppingCart.cs
@@ -5,10 +5,16 @@ namespace ShoppingCart
 {
     public class Cart
     {
-        public float total = 0f;
+        public float totalPrice = 0f;
         public float taxRate = 12.9f;
+        public int totalQuantity = 0;
 
         public void Add(IItem item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public float ComputeTotalWithTax()
         {
             throw new NotImplementedException();
         }
--- ShoppingCartTest/AddItems.cs
+++ ShoppingCartTest/AddItems.cs
@@ -13,13 +13,31 @@ namespace ShoppingCartTest
             Cart cart = new Cart();
             DoveSoap doveSoap = new DoveSoap();
             cart.Add(doveSoap);
-            Assert.Equal(cart.total, doveSoap.price);
+            Assert.Equal(doveSoap.price, cart.totalPrice);
         }
 
         [Fact]
-        public void AddMultipleItems() { }
+        public void AddMultipleItems() 
+        {
+            Cart cart = new Cart();
+            DoveSoap doveSoap_3 = new DoveSoap(quantityIn:3);
+            cart.Add(doveSoap_3);
+            DoveSoap doveSoap_5 = new DoveSoap(quantityIn:5);
+            cart.Add(doveSoap_5);
+            Assert.Equal(doveSoap_5.price * 8, cart.totalPrice); //319.92
+            Assert.Equal(8, cart.totalQuantity);
+        }
 
         [Fact]
-        public void CalculateTax() { }
+        public void CalculateTax() 
+        {
+            Cart cart = new Cart();
+            DoveSoap doveSoap = new DoveSoap(quantityIn:2);
+            cart.Add(doveSoap);
+            AxeDeo axeDeo = new AxeDeo(quantityIn: 2);
+            cart.Add(axeDeo);
+            Assert.Equal(35, cart.ComputeTotalWithTax() - cart.totalPrice);
+            Assert.Equal(314.96, cart.totalPrice);
+        }
     }
 }
